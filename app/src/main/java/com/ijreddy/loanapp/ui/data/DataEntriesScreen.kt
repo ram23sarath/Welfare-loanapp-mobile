a2aa1a96dd26a68a7228442871976b49c4e8f51e
Jaime <ram23sarath@gmail.com>
2026-02-06 17:20:17 +0530

    Fix build: Correct brace imbalance in DataEntriesScreen.kt

--- app/src/main/java/com/ijreddy/loanapp/ui/data/DataEntriesScreen.kt
+++ app/src/main/java/com/ijreddy/loanapp/ui/data/DataEntriesScreen.kt
@@ -118,6 +118,8 @@ fun DataEntriesScreen(
                                 onTap = { },
                                 onLongPress = { }
                             )
+                        }
+                    }
                 }
             }
         }
